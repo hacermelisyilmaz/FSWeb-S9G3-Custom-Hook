89133a6b44e26302d7a4b306a523146b870285fb
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-23 20:20:26 +0300

    localStorageKullan hook is imported to geceModuAc hook

--- frontend/hooks/geceModuAc.js
+++ frontend/hooks/geceModuAc.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { localStorageKullan } from "./localStorageKullan.js";
 
 export const geceModuAc = () => {
   const [geceModu, setGeceModu] = useState(false);
+  localStorageKullan();
   return [geceModu, setGeceModu];
 };
